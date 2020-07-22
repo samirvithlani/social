17dbf59178a9ad13951d9aeafdd31f3f221c19bb
=samirvithlani <=samir.vithlani83955@gmail.com>
2020-07-22 22:16:00 +0530

    fb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SocialLoginModule, SocialAuthServiceConfig, SocialAuthService } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+} from 'angularx-social-login';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule,
   ],
-  providers: [],
+
+  providers: [
+    {
+      provide:'SocialAuthServiceConfig',
+      useValue:{
+        autoLogin:false,
+        providers:[
+          {
+            id:FacebookLoginProvider.PROVIDER_ID,
+            provider:new FacebookLoginProvider('1953004328166658')
+          },
+          {
+            id:GoogleLoginProvider.PROVIDER_ID,
+            provider:new GoogleLoginProvider('957804172312-21akjharehnvleu2h2m3p0re379sqfqb.apps.googleusercontent.com')
+          }
+          
+        ]
+      }as SocialAuthServiceConfig, //data api inject ->
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
